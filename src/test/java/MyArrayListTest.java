25d9a1f5f96f0f8c8bffcf868339e6942f5e0a1f
moreners <alexey.morenets@gmail.com>
2016-11-20 23:50:40 +0200

    + my tests stubs

---
+++ src/test/java/MyArrayListTest.java
@@ -0,0 +1,161 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by a-morenets on 20.11.2016.
+ */
+public class MyArrayListTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void size() throws Exception {
+
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void contains() throws Exception {
+
+    }
+
+    @Test
+    public void iterator() throws Exception {
+
+    }
+
+    @Test
+    public void toArray() throws Exception {
+
+    }
+
+    @Test
+    public void toArray1() throws Exception {
+
+    }
+
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void add1() throws Exception {
+
+    }
+
+    @Test
+    public void remove() throws Exception {
+
+    }
+
+    @Test
+    public void containsAll() throws Exception {
+
+    }
+
+    @Test
+    public void addAll() throws Exception {
+
+    }
+
+    @Test
+    public void addAll1() throws Exception {
+
+    }
+
+    @Test
+    public void removeAll() throws Exception {
+
+    }
+
+    @Test
+    public void retainAll() throws Exception {
+
+    }
+
+    @Test
+    public void clear() throws Exception {
+
+    }
+
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void set() throws Exception {
+
+    }
+
+    @Test
+    public void trimToSize() throws Exception {
+
+    }
+
+    @Test
+    public void ensureCapacity() throws Exception {
+
+    }
+
+    @Test
+    public void remove1() throws Exception {
+
+    }
+
+    @Test
+    public void indexOf() throws Exception {
+
+    }
+
+    @Test
+    public void lastIndexOf() throws Exception {
+
+    }
+
+    @Test
+    public void listIterator() throws Exception {
+
+    }
+
+    @Test
+    public void listIterator1() throws Exception {
+
+    }
+
+    @Test
+    public void subList() throws Exception {
+
+    }
+
+    @Test
+    public void equals() throws Exception {
+
+    }
+
+    @Test
+    public void hashCodeTest() throws Exception {
+
+    }
+
+    @Test
+    public void toStringTest() throws Exception {
+
+    }
+
+}
