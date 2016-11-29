024cdf993a8e89e5313cf54b176d29fa31572baf
moreners <alexey.morenets@gmail.com>
2016-11-30 01:14:20 +0200

    + tests stubs

---
+++ src/test/java/my_hashmap/MyHashMapTest.java
@@ -0,0 +1,77 @@
+package my_hashmap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by a-morenets on 30.11.2016.
+ */
+public class MyHashMapTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void size() throws Exception {
+
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void containsKey() throws Exception {
+
+    }
+
+    @Test
+    public void containsValue() throws Exception {
+
+    }
+
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void put() throws Exception {
+
+    }
+
+    @Test
+    public void remove() throws Exception {
+
+    }
+
+    @Test
+    public void putAll() throws Exception {
+
+    }
+
+    @Test
+    public void clear() throws Exception {
+
+    }
+
+    @Test
+    public void keySet() throws Exception {
+
+    }
+
+    @Test
+    public void values() throws Exception {
+
+    }
+
+    @Test
+    public void entrySet() throws Exception {
+
+    }
+
+}
